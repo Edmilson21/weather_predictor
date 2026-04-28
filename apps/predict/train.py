a96f59a2104a4ec9c751082b5dd56e805efe647e
Edmilson <edmilsoncarlosn94@gmail.com>
2026-04-28 13:02:38 +0200

    bajar la carga del modelo al entrenar222

--- apps/predict/train.py
+++ apps/predict/train.py
@@ -25,7 +25,7 @@ def entrenar_modelo():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     model = RandomForestRegressor(n_estimators=50, random_state=42)
-    model.fit(X_train, y_train)
+    model.fit(X_train, y_train)   
     
     os.makedirs('modelo', exist_ok=True) 
     joblib.dump(model, 'modelo/weather_model.pkl')
